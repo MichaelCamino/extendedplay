3946c292c68203352a0adae53f05ea6e6c5bbd04
Michael <77372266+MichaelCamino@users.noreply.github.com>
2021-11-24 14:52:43 -1000

    works on animations

--- js/index.js
+++ js/index.js
@@ -37,20 +37,20 @@ document.querySelectorAll('#section-4').forEach((div) => {
 });
 
 const animation3 = lottie.loadAnimation({
-    container: strawberrydrink, // the dom element that will contain the animation
+    container: drinkfooter, // the dom element that will contain the animation
     renderer: 'svg',
     loop: false,
     autoplay: false,
-    path: 'js/EP_strawberrydrink/data.json' // the path to the animation json
+    path: 'js/EP_drink_footer/data.json' // the path to the animation json
 });
 
-const io_options_strawberrydrink = {
+const io_options_drinkfooter = {
     // root: document.body,
     rootMargin: '-25% 0px -25% 0px',
     threshold: 0
 };
 
-const myobserver_strawberrydrink = new IntersectionObserver(entries => {
+const myobserver_drinkfooter = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             animation3.play();
@@ -60,9 +60,9 @@ const myobserver_strawberrydrink = new IntersectionObserver(entries => {
 
         }
     });
-}, io_options_strawberrydrink);
+}, io_options_drinkfooter);
 document.querySelectorAll('#section-6').forEach((div) => {
-    myobserver_strawberrydrink.observe(div)
+    myobserver_drinkfooter.observe(div)
 });
 
 // SHAKER ANIMATION
@@ -94,4 +94,66 @@ const myobserver_shaker = new IntersectionObserver(entries => {
 }, io_options_shaker);
 document.querySelectorAll('#section-2').forEach((div) => {
     myobserver_shaker.observe(div)
+});
+
+// TOP LEAF ANIMATION
+
+const animation5 = lottie.loadAnimation({
+    container: topbranch, // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: 'js/EP_topbranch_animation/data.json' // the path to the animation json
+});
+
+const io_options_topbranch = {
+    // root: document.body,
+    rootMargin: '-25% 0px -25% 0px',
+    threshold: 0
+};
+
+const myobserver_topbranch = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            animation5.play();
+
+        } else {
+            animation5.stop();
+
+        }
+    });
+}, io_options_topbranch);
+document.querySelectorAll('#section-1').forEach((div) => {
+    myobserver_topbranch.observe(div)
+});
+
+// BOTTOM BRANCH ANIMATION
+
+const animation6 = lottie.loadAnimation({
+    container: bottombranch, // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: 'js/EP_bottombranch_animation/data.json' // the path to the animation json
+});
+
+const io_options_bottombranch = {
+    // root: document.body,
+    rootMargin: '-25% 0px -25% 0px',
+    threshold: 0
+};
+
+const myobserver_bottombranch = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            animation6.play();
+
+        } else {
+            animation6.stop();
+
+        }
+    });
+}, io_options_bottombranch);
+document.querySelectorAll('#section-1').forEach((div) => {
+    myobserver_bottombranch.observe(div)
 });
