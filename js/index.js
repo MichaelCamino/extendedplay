08f82f7dd5357f9aad30530cc54c9700da473751
Michael <77372266+MichaelCamino@users.noreply.github.com>
2021-12-02 17:25:10 -1000

    edits animations

--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
-const animation1 = lottie.loadAnimation({
-    container: herologo, // the dom element that will contain the animation
-    renderer: 'svg',
-    loop: false,
-    autoplay: true,
-    path: 'js/EP_logo/data.json' // the path to the animation json
-});
-
 // WELCOME ANIMATION
 
 const animation5 = lottie.loadAnimation({
@@ -15,6 +7,18 @@ const animation5 = lottie.loadAnimation({
     autoplay: true,
     path: 'js/EP_welcome_animation/data.json' // the path to the animation json
 });
+
+// LOGO ANIMATION
+
+
+// const animation1 = lottie.loadAnimation({
+//     container: herologo, // the dom element that will contain the animation
+//     renderer: 'svg',
+//     loop: false,
+//     autoplay: true,
+//     path: 'js/EP_logo/data.json' // the path to the animation json
+// });
+
 
 
 // WREATH ANIMATION
@@ -50,64 +54,43 @@ document.querySelectorAll('#section-2').forEach((div) => {
 
 // COFFEE ANIMATION
 
-const animation3 = lottie.loadAnimation({
-    container: coffee, // the dom element that will contain the animation
-    renderer: 'svg',
-    loop: false,
-    autoplay: false,
-    path: 'js/EP_coffee_animation/data.json' // the path to the animation json
-});
+// const animation3 = lottie.loadAnimation({
+//     container: coffee, // the dom element that will contain the animation
+//     renderer: 'svg',
+//     loop: false,
+//     autoplay: false,
+//     path: 'js/EP_coffee_animation/data.json' // the path to the animation json
+// });
 
-const io_options_coffee = {
-    // root: document.body,
-    rootMargin: '-25% 0px -25% 0px',
-    threshold: 0
-};
+// const io_options_coffee = {
+//     // root: document.body,
+//     rootMargin: '-25% 0px -25% 0px',
+//     threshold: 0
+// };
 
-const myobserver_coffee = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            animation3.play();
+// const myobserver_coffee = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             animation3.play();
 
-        } else {
-            animation3.stop();
+//         } else {
+//             animation3.stop();
 
-        }
-    });
-}, io_options_coffee);
-document.querySelectorAll('#section-4').forEach((div) => {
-    myobserver_coffee.observe(div);
-});
+//         }
+//     });
+// }, io_options_coffee);
+// document.querySelectorAll('#section-4').forEach((div) => {
+//     myobserver_coffee.observe(div);
+// });
 
 // DRINK FOOTER
 
 const animation4 = lottie.loadAnimation({
     container: drinkfooter, // the dom element that will contain the animation
     renderer: 'svg',
-    loop: false,
-    autoplay: false,
+    loop: true,
+    autoplay: true,
     path: 'js/EP_drink_footer/data.json' // the path to the animation json
-});
-
-const io_options_drinkfooter = {
-    // root: document.body,
-    rootMargin: '-25% 0px -25% 0px',
-    threshold: 0
-};
-
-const myobserver_drinkfooter = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            animation4.play();
-
-        } else {
-            animation4.stop();
-
-        }
-    });
-}, io_options_drinkfooter);
-document.querySelectorAll('#section-6').forEach((div) => {
-    myobserver_drinkfooter.observe(div)
 });
 
 
